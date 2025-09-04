2825cd86247fbe6c1ba76e5b08eca75be2a0eb43
kauancode <kauancoding@gmail.com>
2025-09-04 14:55:39 -0300

    chore: adicionar variáveis de ambiente e arquivo de configuração do Redis

---
+++ src/config/redis.ts
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+import Redis from 'ioredis'
+
+const redis = new Redis({
+    host: process.env.REDIS_HOST ?? "127.0.0.1",
+    port: Number(process.env.REDIS_PORT) || 6379,
+})
+
+const timestamp = () => new Date().toISOString();
+
+redis.on('connect', ()=> {
+ console.log(
+    `✅ [Redis] Conectado com sucesso em ${redis.options.host}:${redis.options.port} - ${timestamp()}`
+  );})
+
+redis.on("error", (err) => {
+  console.error(`❌ [Redis] Erro de conexão: ${err.message} - ${timestamp}`);
+});
+
+export default redis;
