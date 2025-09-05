b93b2d352791f82ab6a8703c388b499470059e3e
kauancode <kauancoding@gmail.com>
2025-09-05 11:27:18 -0300

    fix(redis): remover timestamp do log de conexão e adicionar constante DEFAULT_CACHE_TTL

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,21 +1,21 @@
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
 import Redis from 'ioredis'
 
+export const DEFAULT_CACHE_TTL = 300;
+
 const redis = new Redis({
     host: process.env.REDIS_HOST ?? "127.0.0.1",
     port: Number(process.env.REDIS_PORT) || 6379,
 })
 
-const timestamp = () => new Date().toISOString();
-
 redis.on('connect', ()=> {
  console.log(
-    `✅ [Redis] Conectado com sucesso em ${redis.options.host}:${redis.options.port} - ${timestamp()}`
+    `✅ [Redis] Conectado com sucesso em ${redis.options.host}:${redis.options.port}}`
   );
 })
 
 redis.on("error", (err) => {
-  console.error(`❌ [Redis] Erro de conexão: ${err.message} - ${timestamp}`);
+  console.error(`❌ [Redis] Erro de conexão: ${err.message}`);
 });
 
 export default redis;
