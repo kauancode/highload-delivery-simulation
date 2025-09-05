7a87b8bfdfac438cae5819b2867f26f9d080001e
kauancode <kauancoding@gmail.com>
2025-09-05 11:29:51 -0300

    feat(server): refatorar inicialização do servidor e conectar ao RabbitMQ

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,26 @@
 import mongoose from "mongoose";
 
 import app from "./app";
+import { connectRabbitMQ } from "./config/rabbitmq";
+import { consumeOrders } from "./queue/orderConsumer";
 
-const PORT = process.env.PORT ?? '3000';
+const PORT = process.env.PORT ?? "3000";
 const MONGO_URI = process.env.MONGO_URI ?? "mongodb://localhost:27017/delivery";
 
-mongoose
-  .connect(MONGO_URI)
-  .then(() => {
-    console.log("✅ MongoDB conectado");
+const start = async () => {
+  try {
+    await mongoose.connect(MONGO_URI);
+
+    await connectRabbitMQ();
+    await consumeOrders();
+
     app.listen(PORT, () => {
-      console.log(`🚀 Servidor rodando na porta ${PORT}`);
+      console.log(`🚀 [Server] Executando na porta ${PORT}`);
     });
-  })
-  .catch((err: unknown) => { console.error("❌ Erro ao conectar no MongoDB", err); });
+  } catch (err) {
+    console.error("❌ [Server] Erro ao iniciar a aplicação", err);
+    process.exit(1);
+  }
+};
+
+await start();
