1b71bbba514facc6397ed17ed4d874219773c0ac
kauancode <kauancoding@gmail.com>
2025-09-15 17:22:10 -0300

    feat(restaurant): adicionar rota para obter todos os restaurantes

--- src/routes/restaurantRoutes.ts
+++ src/routes/restaurantRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 
-import { createRandomRestaurant, createRestaurant, getRestaurantById } from "../controllers/restaurantController";
+import { createRandomRestaurant, createRestaurant, getAllRestaurants, getRestaurantById } from "../controllers/restaurantController";
 
 const router = Router();
 
+router.get("/", getAllRestaurants);
 router.get("/:id", getRestaurantById);
 router.post("/random", createRandomRestaurant);
 router.post("/", createRestaurant);
